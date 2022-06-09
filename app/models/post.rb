5f6d456f3da3f303afd780896af31dc9dd20a0d2
Thi-Ponce <thi.pponce@gmail.com>
2022-06-09 01:02:50 -0300

    add required methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+
+    def most_recent
+        posts.order(created_at: :desc).limit(5)
+    end
+
+    after_save :update_counter
+
+    private
+
+    def update_counter
+        author.increment!(:posts_counter)
+    end
 end
