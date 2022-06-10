50d757ea3e230130ea8d8967bc3724eca733a4e3
Thi-Ponce <thi.pponce@gmail.com>
2022-06-10 18:22:57 -0300

    fix like bug

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,6 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
 
-    return if @post.liked?(current_user)
-
     @like = Like.new(author_id: current_user.id, post_id: @post.id)
 
     if @like.save
