cd5e5ec586cbd6b3ba6c4310ec5f217c29aa79cb
Thi-Ponce <thi.pponce@gmail.com>
2022-06-17 16:48:08 -0300

    create endpoint for create comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,4 +5,15 @@ class Api::V1::CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     render json: @post.comments
   end
+
+  def create
+    user_id = params[:user_id].to_i
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(text: params[:text], post_id: params[:post_id], author_id: params[user_id])
+    if @comment.save
+      render json: {message: 'Comment created!'}
+    else
+      render json: {error: 'Unable to create comment!'}
+    end
+  end
 end
